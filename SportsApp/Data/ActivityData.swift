8f96bf4451a031008a15a3a729524a61ce1d7174
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-17 20:15:02 +0100

    Data Commits

--- SportsApp/Data/ActivityData.swift
+++ SportsApp/Data/ActivityData.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+struct ActivityData  {
+
+    static let ringsPercentage: (movePercentage: Double, exercisePercentage: Double, standPercentage: Double) = (
+    
+        movePercentage: 1.1, exercisePercentage: 1.4, standPercentage: 1.2)
+    // movePercentage: 1.1, exercisePercentage: 1.8, standPercentage: 1.5)
+    
+    static let moveChartData: [Int : Double] = [
+        20: Double.random(in: 0...60),
+        21: Double.random(in: 0...60),
+        22: Double.random(in: 0...60),
+        23: Double.random(in: 0...60),
+        50: Double.random(in: 0...60),
+        51: Double.random(in: 0...60),
+        52: Double.random(in: 0...60),
+        53: Double.random(in: 0...60),
+        54: Double.random(in: 0...60),
+        55: Double.random(in: 0...60),
+        56: Double.random(in: 0...60),
+        57: Double.random(in: 0...60),
+        58: Double.random(in: 0...60),
+        59: Double.random(in: 0...60),
+        60: Double.random(in: 0...60),
+        61: Double.random(in: 0...60),
+        62: Double.random(in: 0...60),
+        63: Double.random(in: 0...60),
+        64: Double.random(in: 0...60),
+        65: Double.random(in: 0...60),
+        90: Double.random(in: 0...60),
+        91: Double.random(in: 0...60),
+        92: Double.random(in: 0...60),
+        93: Double.random(in: 0...60),
+        94: Double.random(in: 0...60),
+        95: Double.random(in: 0...60)
+    
+    ]
+    static let exerciseChartData: [Int: Double] = [
+        30: Double.random(in: 0...60),
+        31: Double.random(in: 0...60),
+        51: Double.random(in: 0...60),
+        52: Double.random(in: 0...60),
+        53: Double.random(in: 0...60),
+        54: Double.random(in: 0...60),
+        55: Double.random(in: 0...60),
+        56: Double.random(in: 0...60),
+        57: Double.random(in: 0...60),
+        80: Double.random(in: 0...60),
+        81: Double.random(in: 0...60),
+        82: Double.random(in: 0...60),
+        83: Double.random(in: 0...60),
+        84: Double.random(in: 0...60),
+        85: Double.random(in: 0...60),
+        86: Double.random(in: 0...60),
+        87: Double.random(in: 0...60),
+        88: Double.random(in: 0...60)
+    ]
+    
+    static let standChartData: [Int: Bool] = [
+        0: true,
+        4: true,
+        15: true,
+        16: true,
+        17: true
+     
+    ]
+    
+    static let steps: String = "5 588"
+    static let distance: String = "4,5"
+    static let flightsClimbed: String = "4"
+}
--- SportsApp/Data/Constant.swift
+++ SportsApp/Data/Constant.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import CoreGraphics
+
+struct  Constants {
+    
+    static let navigationBarHeight: CGFloat = 62
+    
+    static let mainRingThickness: CGFloat = 30
+    
+    static let weekdayRingThickness: CGFloat = 4
+    
+    static let barChartColumns: Int = 96
+    
+    static let barChartColumnWidth: CGFloat = 2.6
+    
+    static let barChartColumnSpacing: CGFloat = 1
+    
+    static let standChartColumns: Int = 24
+    
+    static let standChartColumnWidth: CGFloat = 13.8
+    
+    static let standChartColumnSpacing: CGFloat = 1
+    
+    static let standChartGridColumnSpacing: CGFloat = 2.5
+}
