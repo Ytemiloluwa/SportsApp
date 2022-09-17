4ea27aae6ae419264de1f1baa0dea156c95440d2
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-17 18:29:38 +0100

    Initial Commit

---
+++ SportsApp/SportsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsAppApp.swift
+//  SportsApp
+//
+//  Created by Temiloluwa on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SportsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
